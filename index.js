aaa3fe94410b427c914af8e0f39343a820f40b4e
Pedro Narciso García Revington <p.revington@gmail.com>
2018-02-13 17:05:42 +0100

    v2 (#1)
    * v2

--- index.js
+++ index.js
@@ -1,59 +1,121 @@
 'use strict';
-var update = require('./lib/update');
+const retryLimit = 5;
+const assert = require('assert');
 
-function backoff() {
-    return 300;
+function tooManyErrors(options, count) {
+    return (count + 1) >= options.limit;
 }
 
-function makeRetry(ctx) {
-    return function retry(fn) {
-        var err;
-        ctx.retryLeft--;
-        if (ctx.retryLeft < 0) {
-            return;
+function exponentialBackoff(attempt) {
+    return Math.pow(2, attempt) * 100;
+}
+
+function backOff(fn, time) {
+    setTimeout(fn, time);
+}
+
+function deepCopy(obj) {
+    assert(obj);
+    return JSON.parse(JSON.stringify(obj));
+}
+
+function retry(fn, options, doc, count, callback) {
+    return backOff(function () {
+        return fn(options, doc, count + 1, callback);
+    }, options.backoff(count));
+}
+
+function get(options, doc, count, callback) {
+    options.nano.get(doc._id, function (err, res) {
+        if (err && tooManyErrors(options, count)) {
+            return callback(err);
         }
-        if (!ctx.retryLeft) {
-            err = new Error('too many errors');
-            err.errList = ctx.allErrors;
-            return ctx.callback(err);
+        if (err) {
+            return retry(get, options, doc, count, callback);
         }
-        setTimeout(function () {
-            fn(ctx);
-        }, ctx.options.backoff(ctx.options.maxRetries - ctx.retryLeft));
-    };
+        return options.modify(options, res, count, callback);
+    });
 }
 
-function once(fn) {
-    var called = false;
-    return function () {
-        if (called) {
-            return;
+function update(options, doc, count, callback) {
+    options.nano.insert(doc, function (err, res) {
+        if (err && tooManyErrors(options, count)) {
+            return callback(err);
         }
-        called = true;
-        return fn.apply(fn, arguments);
+        if (err && err.statusCode === 409) {
+            return get(options, doc, count + 1, callback);
+        }
+        if (err) {
+            return retry(update, options, doc, count, callback);
+        }
+        return callback(null, res, count);
+    });
+}
+
+function syncModify(options, doc, count, callback) {
+    let pristineDoc = deepCopy(doc);
+    try {
+        options.transaction(doc);
+    } catch (e) {
+        if (tooManyErrors(options, count)) {
+            return callback(e);
+        } else {
+            return retry(syncModify, options, pristineDoc, count, callback);
+        }
+    }
+    return update(options, doc, count, callback);
+}
+
+function asyncModify(options, doc, count, callback) {
+    let pristineDoc = deepCopy(doc);
+    return options.transaction(doc, function (err, result) {
+        if (err && tooManyErrors(options, count)) {
+            return callback(err);
+        }
+        if (err) {
+            return retry(asyncModify, options, pristineDoc, count, callback);
+        }
+        return update(options, result, count, callback);
+    });
+}
+
+function isAsyncTransaction(transaction) {
+    return transaction.length === 2;
+}
+
+exports = module.exports = function couchDBTransactions(nano, transaction, options) {
+    var myOptions = Object.assign({
+            nano,
+            transaction,
+        }, options),
+        count = 0;
+    myOptions.limit = myOptions.limit || retryLimit;
+    myOptions.modify = isAsyncTransaction(transaction) ? asyncModify : syncModify;
+    myOptions.backoff = myOptions.backoff || exponentialBackoff;
+    return function (idOrDocument, callback) {
+        switch (typeof idOrDocument) {
+        case 'string':
+            {
+                idOrDocument = {
+                    _id: idOrDocument
+                };
+            }
+        case 'object':
+            {
+                let pristineDoc = deepCopy(idOrDocument);
+                return myOptions.modify.call(null, myOptions, pristineDoc, count, callback);
+            }
+        default:
+            {
+                return callback(new Error('idOrDocument should be a string or an object'));
+            }
+        }
     };
-}
-
-function Ctx(obj, callback) {
-    this.allErrors = [];
-    this.retry = makeRetry(this);
-    this.callback = once(callback);
-    this.obj = obj;
-}
-
-function transaction(couchdb, op, options) {
-    options = options || {};
-    options.maxRetries = options.maxRetries || 5;
-    options.backoff = options.backoff || backoff;
-    return function (obj, callback) {
-
-        var ctx = new Ctx(obj, callback);
-        ctx.retryLeft = options.maxRetries;
-        ctx.op = op;
-        ctx.couchdb = couchdb;
-        ctx.options = options;
-
-        update(ctx);
-    };
-}
-exports = module.exports = transaction;
+};
+exports.asyncModify = asyncModify;
+exports.exponentialBackoff = exponentialBackoff;
+exports.get = get;
+exports.isAsyncTransaction = isAsyncTransaction;
+exports.retry = retry;
+exports.syncModify = syncModify;
+exports.update = update;
--- test/acceptance.js
+++ test/acceptance.js
@@ -1,140 +1,236 @@
 'use strict';
+const nano = require('nano');
+const couchdbTxn = require('..');
+const TEST_COUCH_DB = 'http://localhost:5984';
+const assert = require('assert');
+const pkg = require('../package.json');
 
-var transactions = require('..'),
-    http = require('http'),
-    api,
-    crypto = require('crypto'),
-    assert = require('assert'),
-    nano = require('nano');
-
-function sign(obj) {
-    var copy = JSON.parse(JSON.stringify(obj));
-    delete copy._rev;
-    return crypto.createHash('md5').update(JSON.stringify(copy)).digest('base64');
-}
-
-function POSTHandleCreate(req, res) {
-    var obj = req.obj;
-    var signature = req.objSignature;
-    var db = req.db;
-    obj._rev = '1-' + signature;
-    db[obj._id] = obj;
-    res.writeHead(201, {
-        'Content-Type': 'text/plain',
-        'ETag': signature
-    });
-    return res.end(JSON.stringify({
-        ok: true,
-        id: '' + obj._id,
-        rev: signature
-    }));
-}
-
-function POSTHandleUpdate(req, res) {
-    var newRevV = +req.db[req.obj._id]._rev.split('-')[0];
-    var upToDate = req.db[req.obj._id] = req.obj;
-    newRevV++;
-    upToDate._rev = newRevV + '-' + req.objSignature;
-    res.writeHead(201, {
-        'Content-Type': 'text/plain',
-        'ETag': upToDate._rev
-    });
-    return res.end(JSON.stringify({
-        ok: true,
-        id: '' + upToDate._id,
-        rev: upToDate._rev
-    }));
-
-}
-
-function POSTHandleConflict(req, res) {
-    res.writeHead(409);
-    return res.end(JSON.stringify({
-        'error': 'conflict',
-        'reason': 'Document update conflict.'
-    }));
-}
-
-function POST(req, res) {
-    var buffer = '';
-    var db = req.db;
-    req.on('data', function (chunk) {
-        if (chunk) {
-            buffer += chunk.toString();
+function initSandbox(done) {
+    var self = this;
+    self.testDB = pkg.name + '-test-' + Date.now().toString(36);
+    let myDB = nano(TEST_COUCH_DB);
+    myDB.db.create(self.testDB, function (err) {
+        if (err) {
+            return done(err);
         }
-    });
-    req.on('end', function () {
-        var obj = JSON.parse(buffer);
-        var signature = sign(obj);
-        req.obj = obj;
-        req.objSignature = signature;
-
-        if (!db[obj._id]) {
-            return POSTHandleCreate(req, res);
-        }
-        if (obj._rev === db[obj._id]._rev) {
-            return POSTHandleUpdate(req, res);
-        }
-        return POSTHandleConflict(req, res);
+        self.db = myDB.use(self.testDB);
+        return done();
     });
 }
 
-function GET(req, res) {
-    res.end(JSON.stringify(req.db['1']));
+function destroySandbox(done) {
+    var self = this;
+    let myDB = nano(TEST_COUCH_DB);
+    myDB.db.destroy(self.testDB, function (err) {
+        if (err) {
+            console.trace(err.stack);
+            return done(err);
+        }
+        return done();
+    });
 }
-api = {
-    POST: POST,
-    GET: GET
-};
 describe('couchdb-transactions', function () {
-    var db = {};
-    var server = http.createServer((function () {
-        return function (req, res) {
-            req.db = db;
-            api[req.method](req, res);
-        };
-    })());
-
-    function op(doc, cb) {
-        doc.number = doc.number || 0;
-        doc.number++;
-        return cb(null, doc);
-    }
-
+    var doc;
+    var id = 'doc1';
+    var _done = 0;
+    var insertFails = false;
+    var getFails = false;
+    var results = [];
+    before(initSandbox);
+    after(destroySandbox);
     before(function (done) {
-        server.listen(function (err) {
-            var t, _done, db = nano('http://localhost:' + this.address().port + '/test');
+        var db = this.db;
+
+        function sumTransaction(doc) {
+            doc.sum = 1 + (doc.sum || 0);
+        }
+        let txn = couchdbTxn(db, sumTransaction);
+        let oldInsert = db.insert;
+        let oldGet = db.get;
+        db.insert = function (doc, cb) {
+            db.insert = oldInsert;
+            insertFails = true;
+            return cb(new Error('insert error'));
+        };
+        db.get = function (doc, cb) {
+            db.get = oldGet;
+            getFails = true;
+            return cb(new Error('get error'));
+        };
+
+        function cb(err, couchResponse, errorCount) {
+            results.push([err, couchResponse, errorCount]);
+            _done++;
             if (err) {
                 return done(err);
             }
-            t = transactions(db, op);
-            _done = (function () {
-                var left = 3;
-                return function () {
-                    left--;
-                    if (!left) {
-                        return done();
-                    }
-
-                };
-            })();
-            t({
-                _id: '1'
-            }, _done);
-            t({
-                _id: '1'
-            }, _done);
-            t({
-                _id: '1'
-            }, _done);
+            if (_done === 5) {
+                db.get(id, function (err, _doc) {
+                    doc = _doc;
+                    return done();
+                });
+            }
+        }
+        for (let i = 0; i < 5; i++) {
+            txn(id, cb);
+        }
+    });
+    describe('When there are conflicts', function () {
+        it('should retry', function () {
+            assert.deepEqual(doc.sum, 5, JSON.stringify(doc));
         });
     });
-
-    it('should apply all updates', function () {
-        assert.deepEqual(db['1'], {
-            _id: '1',
-            number: 3,
-            _rev: '3-/owPe7doKOg9nw08eK9eVg=='
+    describe('When insert fails', function () {
+        it('should retry', function () {
+            assert(insertFails);
+        });
+    });
+    describe('When get fails', function () {
+        it('should retry', function () {
+            assert(getFails);
+        });
+    });
+    describe('#get(options, doc, count, callback)', function () {
+        describe('on error', function () {
+            var get = couchdbTxn.get;
+            var lastErr;
+            var count = 0;
+            before(function (done) {
+                var options = {};
+                options.nano = {
+                    get: function (id, cb) {
+                        return cb(new Error('err' + (++count)));
+                    },
+                };
+                options.backoff = function () {
+                    return 1;
+                };
+                options.limit = 3;
+                get(options, {}, 0, function (err, result, count) {
+                    lastErr = err;
+                    return done();
+                });
+            });
+            it('should return invoke callback with last error', function () {
+                assert.deepEqual(lastErr.message, 'err3');
+            });
+            it('should retry "options.limit" times', function () {
+                assert(count === 3);
+            });
+        });
+    });
+    describe('#update(options, doc, count, callback)', function () {
+        describe('on error', function () {
+            var update = couchdbTxn.update;
+            var lastErr;
+            var count = 0;
+            before(function (done) {
+                var options = {};
+                options.nano = {
+                    insert: function (id, cb) {
+                        return cb(new Error('err' + (++count)));
+                    },
+                };
+                options.backoff = function () {
+                    return 1;
+                };
+                options.limit = 3;
+                update(options, {}, 0, function (err, result, count) {
+                    lastErr = err;
+                    return done();
+                });
+            });
+            it('should return invoke callback with last error', function () {
+                assert.deepEqual(lastErr.message, 'err3');
+            });
+            it('should retry "options.limit" times', function () {
+                assert(count === 3);
+            });
+        });
+        describe('on document conflict (409)', function () {
+            var update = couchdbTxn.update;
+            var lastErr;
+            var count = 0;
+            var getArgs;
+            before(function (done) {
+                var options = {};
+                options.nano = {
+                    insert: function (id, cb) {
+                        var err = new Error('document update conflict');
+                        err.statusCode = 409;
+                        return cb(err);
+                    },
+                    get: function (id, callback) {
+                        assert(id);
+                        assert(callback);
+                        getArgs = [id, callback];
+                        return done();
+                    }
+                };
+                options.limit = 3;
+                update(options, {
+                    _id: 'some'
+                }, 0, function () {
+                    return done(new Error('unexpected branch'));
+                });
+            });
+            it('should reload document (call #get)', function () {
+                assert.deepEqual(getArgs[0], 'some');
+            });
+        });
+    });
+    describe('#asyncModify(options, doc, count, callback)', function () {
+        describe('on transaction error', function () {
+            var fn = couchdbTxn.asyncModify;
+            var lastErr;
+            var count = 0;
+            before(function (done) {
+                fn({
+                    limit: 3,
+                    backoff: function () {
+                        return 1;
+                    },
+                    transaction: function (doc, cb) {
+                        count++;
+                        return cb(new Error('err' + count));
+                    }
+                }, {}, 0, function (err) {
+                    lastErr = err;
+                    return done();
+                });
+            });
+            it('should retry', function () {
+                assert(count === 3);
+            });
+            it('should give up after too many errors', function () {
+                assert.deepEqual(lastErr.message, 'err3');
+            });
+        });
+    });
+    describe('#syncModify(options, doc, count, callback)', function () {
+        var fn = couchdbTxn.syncModify;
+        var lastErr;
+        var count = 0;
+        before(function (done) {
+            fn({
+                limit: 3,
+                backoff: function () {
+                    return 1;
+                },
+                transaction: function () {
+                    count++;
+                    throw new Error('err' + count);
+                }
+            }, {}, 0, function (err) {
+                lastErr = err;
+                return done();
+            });
+        });
+        it('should retry', function () {
+            assert(count === 3);
+        });
+        it('should give up after too many errors', function () {
+            assert.deepEqual(lastErr.message, 'err3');
         });
     });
 });
